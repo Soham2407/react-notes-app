5998e2c80bbeb4fa2075911a9f1c88d8dc848965
Soham2407 <nesarikarsoham24@gmail.com>
2022-10-02 11:01:12 +0530

    responsive done

--- src/components/NotesForm.js
+++ src/components/NotesForm.js
@@ -3,17 +3,19 @@ import React from "react";
 const NotesForm = ({ value, setValue, submitHandler, isEdit }) => {
   return (
     <form onSubmit={submitHandler}>
-      <input
-        type="text"
-        placeholder="Enter Note"
-        value={value}
-        required
-        className="p-1 w-50"
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <button type="submit" className="btn btn-primary mb-1">
-        {isEdit ? "Edit" : "Add"}
-      </button>
+      <div className="add-items">
+        <input
+          type="text"
+          placeholder="Enter Note"
+          value={value}
+          required
+          className="p-1 w-75"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit" className="btn btn-primary mb-1">
+          {isEdit ? "Edit" : "Add"}
+        </button>
+      </div>
     </form>
   );
 };
